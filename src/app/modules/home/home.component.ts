ffcab88c6d95bea9a53ffc979832ae1da0e114bc
unknown <fernandesgabriel1227@gmail.com>
2024-08-09 14:21:03 -0300

    Adicionando meu formulário reativo

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Password } from 'primeng/password';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard  = true
+  loginForm:FormGroup
+  signupForm:FormGroup
+
+  onSubmitLoginForm():void{
+    console.log("DADOS  DO FORMULÁRIO DE LOGIN", this.loginForm.value)
+  }
+
+  onSubmitSignupForm():void{
+    console.log("DADOS DO FORMULÁRIO DE CRIAÇÃO DE CONTA", this.signupForm.value)
+  }
+
+  constructor(private formBuilder: FormBuilder){
+    this.loginForm = this.formBuilder.group({
+      email:['',Validators.required],
+      password:['',Validators.required]
+    })
+
+    this.signupForm = this.formBuilder.group({
+      name:['',Validators.required],
+      email:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
 }
