dac42928e01f8b4a2c158faf8d8266814ce11cf3
Fox Cloud <foxclouderp@gmail.com>
2024-08-14 17:27:41 -0300

    Adicionando minhas dependências ao  meu modúlo  de produto

--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ProductsService } from '../../../../Services/products/products.service';
+import { ProductsDataTransferService } from '../../../../sherad/services/products/products-data-transfer.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products-home',
   templateUrl: './products-home.component.html',
 })
-export class ProductsHomeComponent {
+export class ProductsHomeComponent implements OnDestroy {
+    private readonly  destroy$: Subject<void>  =  new  Subject()
+
+    constructor(
+      private  productsService:ProductsService,
+      private  productsDtService:ProductsDataTransferService,
+      private  router:Router
+    ){}
+
+  ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete
+  }
 
 }
